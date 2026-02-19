3d61d43c5da45c03b2530d3ad537b83a1fcbfc60
Peiman Farajnezhad <peiman.farajnezhad@gmail.com>
2026-02-19 21:36:33 +0100

    feat: add deploy workflow to deploy on pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/tv-maze-explorer/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
